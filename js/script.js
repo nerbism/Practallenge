7ec3e1ca97af62d4dda34c1bdcb7c19f4c59223f
nerbism <roadtobren@gmail.com>
2023-12-13 04:20:16 -0700

    what is js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // JavaScript Learning 
+
+// (1) What is JavaScript?
+        // JavaScript is a programming language.
+        // Its a language that tells your computer what to do.
+        // A language that makes websites interactive and dynamic.
+        
