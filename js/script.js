6ad70a2dbdd3f0b82a5e58a5413682c08d5def1d
nerbism <roadtobren@gmail.com>
2023-12-13 19:52:14 -0700

    js s6 fix

--- js/script.js
+++ js/script.js
@@ -100,21 +100,21 @@
 ///////////////
 
 // (1) The DOM
-// (1) Intro to the DOM
-// (1) Document Element Propeerties 
-// (1) DOM Selectors - Single Elements 
-// (1) DOM Selectors - Multiple Elements 
-// (1) Traversing the DOM - Elements 
-// (1) Traversing the DOM - Nodes 
-// (1) Create and Append Elements 
-// (1) innerHTML vs CreateElement
-// (1) Refactor to Multiple Functions
-// (1) Insert Elements, Text, and HTML
-// (1) Custom insertAfter() CHallenge 
-// (1) Replacing Elments 
-// (1) Removing Elements 
-// (1) Working with Styles & Classes
-// (1) What is JavaScript?
+// (2) Intro to the DOM
+// (3) Document Element Propeerties 
+// (4) DOM Selectors - Single Elements 
+// (5) DOM Selectors - Multiple Elements 
+// (6) Traversing the DOM - Elements 
+// (7) Traversing the DOM - Nodes 
+// (8) Create and Append Elements 
+// (9) innerHTML vs CreateElement
+// (10) Refactor to Multiple Functions
+// (11) Insert Elements, Text, and HTML
+// (12) Custom insertAfter() CHallenge 
+// (13) Replacing Elments 
+// (14) Removing Elements 
+// (15) Working with Styles & Classes
+// (16) What is JavaScript?
 
 
 
