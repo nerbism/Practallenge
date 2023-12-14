ac159d2e6f27af825264fddfa86e28045d28ff89
nerbism <roadtobren@gmail.com>
2023-12-13 19:56:42 -0700

    js s7 Qs

--- js/script.js
+++ js/script.js
@@ -116,10 +116,22 @@
 // (15) Working with Styles & Classes
 // (16) What is JavaScript?
 
+///////////////
+// SECTION 7 //
+///////////////
 
+// (1) Events 
+// (2) Event Listeners 
+// (3) Mouse Events 
+// (4) the Event Object 
+// (5) Keyboard Events an Key Properties
+// (6) KeyCode Mini-Project 
+// (7) Input Events 
+// (8) Form Submission & Form Object 
+// (9) Event Bubbling
+// (10) Event Delegatoion & Multiple Events 
+// (11) Page Loadding and Window Events
 
-// (1) What is JavaScript?
-// (1) What is JavaScript?
 
 
 
