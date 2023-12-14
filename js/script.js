52977dc28201728758fcb716c4439860044e0097
nerbism <roadtobren@gmail.com>
2023-12-13 19:10:22 -0700

    js s1 Qs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,65 @@
 // JavaScript Learning 
 
+///////////////
+// SECTION 1 //
+///////////////
+
 // (1) What is JavaScript?
         // JavaScript is a programming language.
         // Its a language that tells your computer what to do.
         // A language that makes websites interactive and dynamic.
-        
+
+
+// (2) Enviroment Setup.
+// (3) Running JS in the browser.
+// (4) Using the Sanbox.
+// (5) The Browser Console.
+// (6) Comments & Shortcuts.
+// (7) Variables and Constants.
+// (8) Data Types.
+// (9) Primittive vs Reference.
+// (10) Type Conversion.
+// (11) Arithmettic & Comparison Operators .
+// (12) Type Coercion.
+// (13) Strings 
+// (14) Capitalize Challenge
+// (15) Numbers 
+// (16) Math Object
+// (17) Number Challenge 
+// (18) Date Object 
+// (19) Date Object Methods 
+
+///////////////
+// SECTION 2 //
+///////////////
+
+// (1) 
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+// (1) What is JavaScript?
+
