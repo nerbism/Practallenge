2024cc2109dd8fe415f54dab54ef743960afc6f2
nerbism <roadtobren@gmail.com>
2023-12-17 23:20:38 -0700

    html files sections 1-7

---
+++ js/s1/script.js
@@ -0,0 +1,36 @@
+// JavaScript Learning 
+
+///////////////
+// SECTION 1 //
+///////////////
+
+// (1) What is JavaScript?
+        // JavaScript is a programming language.
+        // Its a language that tells your computer what to do.
+        // A language that makes websites interactive and dynamic.
+
+// (2) Enviroment Setup.
+        // Install git
+        // Install nodejs
+
+// (3) Running JS in the browser.
+        // Use Live Server in the browser, no need to reload.
+
+// (4) Using the Sandbox.
+
+
+// (5) The Browser Console.
+// (6) Comments & Shortcuts.
+// (7) Variables and Constants.
+// (8) Data Types.
+// (9) Primittive vs Reference.
+// (10) Type Conversion.
+// (11) Arithmettic & Comparison Operators .
+// (12) Type Coercion.
+// (13) Strings 
+// (14) Capitalize Challenge
+// (15) Numbers 
+// (16) Math Object
+// (17) Number Challenge 
+// (18) Date Object 
+// (19) Date Object Methods 
