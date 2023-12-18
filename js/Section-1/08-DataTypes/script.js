3f8d5f51c3e15efa40dab73c17f85eb5a5906ef8
nerbism <roadtobren@gmail.com>
2023-12-18 03:04:27 -0700

    Primitive Data Types

---
+++ js/Section-1/08-DataTypes/script.js
@@ -0,0 +1,52 @@
+// Data Types
+
+    // Primitive Data Types:
+        /*
+            Strings - 'Bren'
+            Numbers - 100
+            Booleans - True 
+            Null - (empty)
+            Undefined  - Variables that have yet to be assigned.
+            Symbol -
+            BigInt - Numbers that are extremely large.
+        */
+            // Strings
+            const firstName = 'Bren';
+            const nameOutput = firstName;
+            console.log(nameOutput)
+            console.log(nameOutput, typeof nameOutput)
+
+
+            // Numbers
+            const age = 25
+            const ageOutputt = age
+            console.log(ageOutputt)
+            console.log(ageOutputt, typeof ageOutputt)
+
+
+            // Booleans 
+            const hasKids = false 
+            const kidsOutput = hasKids
+            console.log(kidsOutput)
+            console.log(kidsOutput, typeof kidsOutput)
+
+
+            // Undefined
+            let bodyCount;
+            console.log(bodyCount)
+
+
+            // Symbol 
+            const id = Symbol('id')
+            console.log(id)
+            console.log(typeof id)
+
+
+            
+    // Reference Data Types 
+        /*
+            Objects
+            Arrays 
+            Functions 
+
+        */
