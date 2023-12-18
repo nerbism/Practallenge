d111835f1ac72722122595aa8c06f1204e2abe1d
nerbism <roadtobren@gmail.com>
2023-12-17 23:25:04 -0700

    s5 js

---
+++ js/s5/script.js
@@ -0,0 +1,18 @@
+
+///////////////
+// SECTION 5 //
+///////////////
+
+// (1) Loops & High Order Array Functions 
+// (2) For Loop
+// (3) Break and Continue 
+// (4) While & Do  While Loops 
+// (5) FizzBuzz Challenge 
+// (6) For... Of Loop
+// (7) For... In Loop 
+// (8) Array.forEach
+// (9) Arrray.filter
+// (10) Array.map
+// (11) Array.reduce 
+// (12) Array Method Challenges 
+// (13) What is JavaScript?
