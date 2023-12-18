10b3abc7e51a5b514e069801de7a404218cd6915
nerbism <roadtobren@gmail.com>
2023-12-17 23:27:21 -0700

    s7 js

---
+++ js/s7/script.js
@@ -0,0 +1,18 @@
+
+
+///////////////
+// SECTION 7 //
+///////////////
+
+// (1) Events 
+// (2) Event Listeners 
+// (3) Mouse Events 
+// (4) the Event Object 
+// (5) Keyboard Events an Key Properties
+// (6) KeyCode Mini-Project 
+// (7) Input Events 
+// (8) Form Submission & Form Object 
+// (9) Event Bubbling
+// (10) Event Delegatoion & Multiple Events 
+// (11) Page Loadding and Window Events
+
