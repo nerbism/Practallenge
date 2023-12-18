3122ad659ae535615ae838e2ea59c0054914f4e0
nerbism <roadtobren@gmail.com>
2023-12-17 23:24:31 -0700

    s4 js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
 
-
-
-///////////////
-// SECTION 4 //
-///////////////
-
-// (1) Logic and Control FLow
-// (2) If Statements 
-// (3) If else & Nesting
-// (4) Switch Statements 
-// (5) Calculator Challenge
-// (6) Truthy and Falsy Values 
-// (7) Logical Operators 
-// (8) Logical Assignment
-// (9) Tenary Operator
-
 ///////////////
 // SECTION 5 //
 ///////////////
