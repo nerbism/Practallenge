941fb02337fc0811a0e6782f1d5942b56761a726
nerbism <roadtobren@gmail.com>
2023-12-18 00:37:04 -0700

    s105p1

---
+++ js/Section-1/05-BrowserConsole/script.js
@@ -0,0 +1,12 @@
+// The Browser Console 
+
+    // To get to the console in the browser:
+        // Go to the browwser 
+        // Mac: cmd opt i 
+        // Windows: F12
+
+    // Then hit the console tab -> You are now in the browser console.
+        // From the Browser Console you are able to write JavaScript right in that console. 
+        // The code is not perm of course, once the page is refreshed that code will vanish.
+
+    // Logging to the console:
