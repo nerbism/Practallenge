8b24ec72c1bfdcd6aada1790b8bb939d33325f8d
nerbism <roadtobren@gmail.com>
2023-12-17 23:22:35 -0700

    section 2 js

---
+++ js/s2/script.js
@@ -0,0 +1,15 @@
+
+///////////////
+// SECTION 2 //
+///////////////
+
+// (1) Arrays and Objects.
+// (2) Array Basics
+// (3) Array Methods 
+// (4) Nesting, Concat, Spread
+// (5) Array Challenges
+// (6) Object Literals
+// (7) Object Sprea Operator 
+// (8) Destructuring and Naming
+// (9) JSON Intro
+// (10) Object Challenge 
