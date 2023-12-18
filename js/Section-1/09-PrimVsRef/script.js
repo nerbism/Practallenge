dac7207e4dcc664127d9c3df2c828eee4880f26d
nerbism <roadtobren@gmail.com>
2023-12-18 03:36:43 -0700

    PvR data types

---
+++ js/Section-1/09-PrimVsRef/script.js
@@ -0,0 +1,5 @@
+// Prim Vs Reference Data Types 
+
+    // Primitiive types of data are stored in a data structure called the stack
+
+    //Reference Data Types ar einside of a datta structure named the heap.
