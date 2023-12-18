e6555442cb856e7062b0b3f1ba4697a08e32e420
nerbism <roadtobren@gmail.com>
2023-12-17 23:23:30 -0700

    s3 js

---
+++ js/s3/script.js
@@ -0,0 +1,18 @@
+
+///////////////
+// SECTION 3 //
+///////////////
+
+// (1) Functions, Scope, and Execution
+// (2) Function Basics 
+// (3) Parameters & Arguments 
+// (4) Global & Function Scope
+// (5) Block Scope 
+// (6) Nested Scope 
+// (7) Declaration vs Expression
+// (8) Arrow Functions 
+// (9) IFEE
+// (10) Function Challenges 
+// (11) Execution Context
+// (12) Execution Context in Action
+// (13) The Call Stack
--- js/script.js
+++ js/script.js
@@ -1,38 +1,5 @@
 
 
-///////////////
-// SECTION 2 //
-///////////////
-
-// (1) Arrays and Objects.
-// (2) Array Basics
-// (3) Array Methods 
-// (4) Nesting, Concat, Spread
-// (5) Array Challenges
-// (6) Object Literals
-// (7) Object Sprea Operator 
-// (8) Destructuring and Naming
-// (9) JSON Intro
-// (10) Object Challenge 
-
-
-///////////////
-// SECTION 3 //
-///////////////
-
-// (1) Functions, Scope, and Execution
-// (2) Function Basics 
-// (3) Parameters & Arguments 
-// (4) Global & Function Scope
-// (5) Block Scope 
-// (6) Nested Scope 
-// (7) Declaration vs Expression
-// (8) Arrow Functions 
-// (9) IFEE
-// (10) Function Challenges 
-// (11) Execution Context
-// (12) Execution Context in Action
-// (13) The Call Stack
 
 ///////////////
 // SECTION 4 //
