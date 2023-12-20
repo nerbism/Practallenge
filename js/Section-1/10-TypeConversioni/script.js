62dfc8ddae7b52c2d1d58e7eb95632d95b99f0e1
nerbism <roadtobren@gmail.com>
2023-12-20 01:11:00 -0700

    nmbr to string

--- js/Section-1/10-TypeConversioni/script.js
+++ js/Section-1/10-TypeConversioni/script.js
@@ -1,7 +1,18 @@
 // Type Conversion
 
+
+// Change String into a Number
 let nmbr = '999'
 console.log(nmbr, typeof nmbr)
 
 nmbr = Number(nmbr)
 console.log(nmbr, typeof nmbr)
+
+
+
+// Change Number into a String
+
+let random = 700;
+
+random  = random.toString();
+console.log(random, typeof random)
