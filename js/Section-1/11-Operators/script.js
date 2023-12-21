06d21529236e34bf9d42f64baa0322f68821b2e7
nerbism <roadtobren@gmail.com>
2023-12-20 21:24:05 -0700

    operators

---
+++ js/Section-1/11-Operators/script.js
@@ -0,0 +1,35 @@
+// Arithmetic and Comparrison Operators 
+
+// Arithmetic Operators
+ a = 5 + 5 
+ console.log(a)
+ b = 5 - 3 
+ console.log(b)
+ c = 6 / 2
+ console.log(c)
+ d = 6 * 2
+ console.log(d)
+ e = 5 % 20
+ console.log(e)
+
+
+// Concatenation 
+f ='Hello' + ' World'
+console.log(f)
+
+//Exponent 
+//To the power
+g = 2 ** 3
+console.log(g)
+
+// Increment
+h = 1 
+h++
+console.log(h)
+
+// Assignment Operators 
+i = 10
+i =  i + 5
+console.log(i)
+// or
+i += 5 
