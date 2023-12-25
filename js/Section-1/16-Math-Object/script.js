d5274eac6a9b6d44d82a37632ad94bd586645219
nerbism <roadtobren@gmail.com>
2023-12-25 03:03:11 -0700

    math object

--- js/Section-1/16-Math-Object/script.js
+++ js/Section-1/16-Math-Object/script.js
@@ -1,1 +1,21 @@
 // The Math Object 
+
+let x;
+
+x = Math.round(1.5)
+// Rounds the number normally.
+
+x = Math.sqrt(9)
+// Grabs the squaree root of 9.
+
+x = Math.floor(1.9)
+// Rounds down a number.
+
+x = Math.ceil(1.2)
+// Rounds a number up.
+
+x = Math.floor(Math.random() * 10 + 1 )
+// Grabs a random number bewtween 0 and 10.
+
+
+console.log(x)
