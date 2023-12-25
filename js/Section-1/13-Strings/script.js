8ffc77858aac7d6a5b0b41ef59d27df17ae25ec0
nerbism <roadtobren@gmail.com>
2023-12-25 01:30:14 -0700

    string props and methods

--- js/Section-1/13-Strings/script.js
+++ js/Section-1/13-Strings/script.js
@@ -23,3 +23,33 @@ y = `Hello my name is ${name} and I am ${age} years old!`
 console.log(y)
 // A cleaner and easier way.
 
+
+
+
+// String properties and Method
+let s;
+
+s = 'Hello World'
+
+x = s.length;
+// Shows the number of characters (length) in a string.
+console.log(x)
+
+
+
+
+x = s.indexOf('H');
+// Shows the index of the inputted letter (First it encounters)
+console.log(x)
+
+x = s.toUpperCase()
+// Capitalizes the strirng as a whole.
+console.log(x)
+
+
+x = s.toLowerCase()
+// Lower cases the sting as a whole.
+console.log(x)
+
+x = s.trim()
+// Removes white space from a string (Non neeed spaces.)
