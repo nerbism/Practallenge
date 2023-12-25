2b7161268af972e63575790b5a8f417702916983
nerbism <roadtobren@gmail.com>
2023-12-25 01:04:12 -0700

    Template literals

--- js/Section-1/13-Strings/script.js
+++ js/Section-1/13-Strings/script.js
@@ -9,3 +9,17 @@ const age = 25
 
 x = 'Hello my name is ' + name + ' and I am ' + age + 'years old'
 console.log(x)
+
+
+
+
+
+
+// Template Literals / Template Strings
+
+let y;
+
+y = `Hello my name is ${name} and I am ${age} years old!`
+console.log(y)
+// A cleaner and easier way.
+
