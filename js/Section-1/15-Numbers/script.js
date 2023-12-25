1d50691ee0de6b65ea2ac5d877f93ab108706550
nerbism <roadtobren@gmail.com>
2023-12-25 02:34:54 -0700

    numbers

--- js/Section-1/15-Numbers/script.js
+++ js/Section-1/15-Numbers/script.js
@@ -1,1 +1,19 @@
 // Numbers 
+
+const num = new Number(10);
+console.log(num)
+
+// Methods
+
+let x;
+x = num.toString()
+console.log(x)
+// Changes the type from number to a String
+
+
+x = num.toString().length
+console.log(x)
+// You can't get length from a Number. So You will have to switch it to a string then grab the length.
+
+
+
