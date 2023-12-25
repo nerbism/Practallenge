48a1f7f509953ecafaa2f7b6b99072790c36c40b
nerbism <roadtobren@gmail.com>
2023-12-25 03:17:26 -0700

    math challenge

--- js/Section-1/17-CHALLENGE/script.js
+++ js/Section-1/17-CHALLENGE/script.js
@@ -1,2 +1,32 @@
 // Number Challenge
 
+//Create a random number between 1 and 100 
+    let x;
+    x = Math.floor(Math.random() * 100 + 1 )
+    console.log(x)
+
+//Create a random number between 1 and 50
+    let y;
+    y = Math.floor(Math.random() * 50 + 1 )
+    console.log(y)
+
+
+// Sum of random numbers 
+    const sumOutput = x + y
+    console.log(sumOutput)
+
+// Difference of randdom numbers 
+    const diffOutput = x - y 
+    console.log(diffOutput)
+
+// Product of randdom numbers
+    const prodOutput = x * y 
+    console.log(prodOutput)
+
+// Quotient of random numbers 
+    const quotOutput = x / y 
+    console.log(quotOutput)
+
+// Remaining of random numbers 
+    const remOutput = x % y 
+    console.log(remOutput)
