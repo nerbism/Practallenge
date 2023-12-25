dd2b8134e4b618574d75a6f1cec84a538c6edcb6
nerbism <roadtobren@gmail.com>
2023-12-25 02:22:24 -0700

    Better solution

--- js/Section-1/14-CHALLENGE/script.js
+++ js/Section-1/14-CHALLENGE/script.js
@@ -3,10 +3,19 @@
 const myString = 'developer'
 //GOAL: Conole to log 'Developer
 
+
+
+// let x;
+// x = myString.substring(1)
+// console.log(x)
+// let y;
+// y = 'D'
+// myNewString = y + x
+// console.log(myNewString)
+
+
+
+// Other Solutions
 let x;
-x = myString.substring(1)
+x = myString.charAt(0).toUpperCase() + myString.substring(1)
 console.log(x)
-let y;
-y = 'D'
-myNewString = y + x
-console.log(myNewString)
