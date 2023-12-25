4f81ac550a8a9902d92b124252cf95381ce45492
nerbism <roadtobren@gmail.com>
2023-12-25 02:13:16 -0700

    Challenge complete

--- js/Section-1/14-CHALLENGE/script.js
+++ js/Section-1/14-CHALLENGE/script.js
@@ -1,2 +1,12 @@
 // Capitalize Challenge
 
+const myString = 'developer'
+//GOAL: Conole to log 'Developer
+
+let x;
+x = myString.substring(1)
+console.log(x)
+let y;
+y = 'D'
+myNewString = y + x
+console.log(myNewString)
