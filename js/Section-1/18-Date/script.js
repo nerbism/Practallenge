46647e639ccb7d6311843af2453283642e387954
nerbism <roadtobren@gmail.com>
2023-12-25 03:36:38 -0700

    date obj

--- js/Section-1/18-Date/script.js
+++ js/Section-1/18-Date/script.js
@@ -1,2 +1,25 @@
 // Date Object 
 
+let d;
+
+d = new Date()
+console.log(d)
+// Shows current date in the console.
+
+
+
+d = new Date(2024, 6, 10)
+console.log(d)
+// Setting a new date.
+
+
+
+d = new Date(2024, 6, 10, 12,32, 59)
+console.log(d)
+// Setting a new date with time.
+
+
+d = new Date('07/10/2024 12:32:59')
+console.log(d)
+// Same code as above with a different format.
+
