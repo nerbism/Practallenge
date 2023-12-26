2b1957574a93b502a6db4714ed4be37805205e3e
nerbism <roadtobren@gmail.com>
2023-12-26 00:15:49 -0700

    Manipulating Arrays

--- js/Section-2/02-ArrayMethods/script.js
+++ js/Section-2/02-ArrayMethods/script.js
@@ -1,2 +1,37 @@
 // Array Methods
 
+const favLifts = ['Squats', 'Incline Bench', 'Chest Flies']
+
+// Manipulating the Array \\
+
+//  (1)
+favLifts.push('Sissy Squats')
+// Method that adds to the end of the Array
+console.log('(Example 1)',favLifts)
+
+
+//  (2)
+favLifts.pop()
+// Removes from the end of the Array
+console.log('Example 2)',favLifts)
+
+
+//  (3)
+favLifts.unshift('Calve Raises')
+// Adds to the 0 index of the Array.
+console.log('(Example 3)',favLifts)
+
+
+//  (4)
+favLifts.shift()
+// Removes the current 0 index.
+console.log('(Example 4)',favLifts)
+
+
+
+//  (5)
+favLifts.reverse()
+// Reverses the Array. 
+console.log('Example 5', favLifts)
+
+console.log(favLifts)
