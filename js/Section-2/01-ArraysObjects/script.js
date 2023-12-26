71e25a625a17f3f166eab39e303fe6a58c6011b8
nerbism <roadtobren@gmail.com>
2023-12-25 23:56:33 -0700

    basic arrays

--- js/Section-2/01-ArraysObjects/script.js
+++ js/Section-2/01-ArraysObjects/script.js
@@ -1,3 +1,23 @@
 // Arrays and Objects
 
 
+// Arrays \\
+let x;
+// Array Literals 
+const numbers = [12, 68, 69, 55]
+x = numbers[0]
+console.log(x)
+
+x = numbers[0] + numbers[1]
+console.log(x)
+
+// Array Consttructor
+ const cars = new Array('BMW M3', 'Porsche GT3RS', 'Lamborghini Urus')
+ let y;
+
+y = `My dream daily driver is a ${cars[0]}.`
+console.log(y)
+
+
+
+// Objects 
