338cf6eb38229952b9cf84a4ab32ffeaa205e9fa
nerbism <roadtobren@gmail.com>
2023-12-26 02:04:32 -0700

    Challenge 2 array

--- js/Section-2/04-CHALLENGE/script.js
+++ js/Section-2/04-CHALLENGE/script.js
@@ -1,7 +1,18 @@
 // Array Challenge 
-// Challenge 1 
+// Challenge 1 \\
 const arr = [1, 2, 3, 4, 5]
 // Make the output = [6, 5, 4, 3, 2 , 1, 0]
 
 arr.push(6), arr.unshift(0), arr.reverse()
 console.log(arr)
+
+
+// Challenge 2 \\
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [5, 6, 7, 8, 9, 10]
+// Make arr3 outputt: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+const arr3 = [...arr1, ...arr2]
+arr3.splice(4, 1)
+console.log(arr3)
