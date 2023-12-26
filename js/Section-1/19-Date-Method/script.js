ab7f764797fc98a6b851dd78e95a83cd8ef6f5ae
nerbism <roadtobren@gmail.com>
2023-12-25 17:39:23 -0700

    type in h1

--- js/Section-1/19-Date-Method/script.js
+++ js/Section-1/19-Date-Method/script.js
@@ -1,1 +1,33 @@
 // Date Methods
+let x;
+let d = new Date();
+console.log(d)
+
+x = d.getDate()
+console.log(x)
+// Grabs the day of the Month 
+
+
+x = d.getFullYear()
+console.log(x)
+// Grabs the year of the current date.
+
+x = d.getMonth()
+console.log(x)
+// Grabs the month of the current date.
+
+x = d.getDay()
+console.log(x)
+// Gives the day of the week.
+
+x = d.getHours()
+console.log(x)
+// Grabs the current hours.
+
+x = d.getMinutes()
+console.log(x)
+// Grabs the current minutes of the day.
+
+
+x = d.getSeconds()
+console.log(x)
