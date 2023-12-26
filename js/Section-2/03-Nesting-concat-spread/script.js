f556fff73f78d7bcc9984de997ba3d56a84ee819
nerbism <roadtobren@gmail.com>
2023-12-26 01:04:13 -0700

    Nestting Arrays

--- js/Section-2/03-Nesting-concat-spread/script.js
+++ js/Section-2/03-Nesting-concat-spread/script.js
@@ -1,1 +1,22 @@
 // Nesting, Concat, Spread
+let x;
+
+
+// Array Nesting \\
+
+cars = []
+germanCars = ['BMW M3', 'Porsche GT3', 'Audi RS7', 'Mercedes GTR'];
+japCars = ['Lexus LFA', 'Subaru BRZ', 'Honda S2000', 'Nissan GTR']
+
+cars.push(germanCars, japCars)
+console.log(cars)
+// Now we have An Array inside of an Array.
+
+// Grab BMW out of the cars Array.
+x = cars[0][0]
+console.log(x)
+
+
+// Grab Nissan GTR out of the cars Array.
+x = cars[1][3]
+console.log(x)
