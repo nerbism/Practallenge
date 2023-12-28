4ac57f1221106023c0b5fd57438d0b7cbfaaf560
nerbism <roadtobren@gmail.com>
2023-12-27 22:40:26 -0700

    extracting and destructuring

--- js/Section-2/07-Destructuring/script.js
+++ js/Section-2/07-Destructuring/script.js
@@ -1,1 +1,14 @@
 // Destructuring and Naming
+const coolestPerson = {
+    id: 999,
+    name: 'Bren',
+    height: `6'3`,
+    weight: '200lbs',
+    hobbies: ['Bodybuilding', 'Coding', 'Reading']
+}
+
+// Now, instead of writing something like: id = coolestPerson.id I can extract or destructure the information inside the objecct:
+
+    const { id, name } = coolestPerson
+    console.log(id)
+    console.log(name)
