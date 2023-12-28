2e4b3e65e7cdfcf8f3fa79f36b3dc6bc2e66da78
nerbism <roadtobren@gmail.com>
2023-12-27 22:04:39 -0700

    spread on objects

--- js/Section-2/06-ObjectSpread/script.js
+++ js/Section-2/06-ObjectSpread/script.js
@@ -8,7 +8,29 @@ x = todo
 todo.id = 1
 todo.job = 'Software Engineer'
 todo.task = 'Homework'
+todo.completed = false
 
 
+console.log(x)
 
+
+const obj = {
+    a : 1,
+    b : 2,
+
+}
+
+const obj2 = {
+    c : 3,
+    d : 4,
+
+}
+
+
+const obj3 = {
+    ...obj,
+    ...obj2
+}
+
+x = obj3
 console.log(x)
