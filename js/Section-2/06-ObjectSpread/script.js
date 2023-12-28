8d1026aefcd5d682a3bcdd0b9c3b9a4834cc3dec
nerbism <roadtobren@gmail.com>
2023-12-27 22:27:05 -0700

    Objct Spread

--- js/Section-2/06-ObjectSpread/script.js
+++ js/Section-2/06-ObjectSpread/script.js
@@ -13,6 +13,22 @@ todo.completed = false
 
 console.log(x)
 
+// Logging keys from an Object -
+   x = Object.keys(todo)
+   console.log(x)
+
+// Logging values from an Object -
+   x = Object.values(todo)
+   console.log(x)
+
+// Logging key and value pairs from an Object -
+    x = Object.entries(todo)
+    console.log(x)
+
+
+// Seeing if an objeect has a propertty
+x = todo.hasOwnProperty('name')
+console.log(x)
 
 const obj = {
     a : 1,
@@ -34,3 +50,16 @@ const obj3 = {
 
 x = obj3
 console.log(x)
+
+// Objects in Arrays.
+
+const todos = [
+    {id:1, name: 'Milk'},
+    {id:2, name: 'Ice Cream'},
+    {id:3, name: 'Protein Powder'},
+    {id:4, name: 'Egg Whites'},
+]
+
+
+
+x 
