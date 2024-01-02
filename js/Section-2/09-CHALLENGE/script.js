bf3fa541c95fda6ac43f1ed7247010a57b8b8a8f
nerbism <roadtobren@gmail.com>
2024-01-02 03:11:54 -0700

    objCHallenge s1

---
+++ js/Section-2/09-CHALLENGE/script.js
@@ -0,0 +1,39 @@
+// Object Literal Challenge 
+
+// Step 1:
+// Create an Array of objects called library.
+// 3 objects with a property of title, author, and status
+// Title & Author should be strings
+// Statuts should be a nested object with the properties of own, reading ,and read: all booleans
+// Own to True and reading and read to false
+
+
+const library = [
+    {
+        title: 'Think & Grow Rich',
+        author: 'Napolean Hill',
+        status: {
+            own: true,
+            reading: false,
+            read: false,
+        }
+    },
+    {
+        title: 'The Art of War',
+        author: 'Sun Tzu',
+        status: {
+            own: true,
+            reading: false,
+            read: false,
+        }
+    },
+    {
+        title: 'Thinking Clearly',
+        author: 'Rolf Dobeelli',
+        status: {
+            own: true,
+            reading: false,
+            read: false,
+        }
+    }
+]
