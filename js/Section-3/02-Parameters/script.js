ea4f8d290dea006d8a6d9dfc1bba590a2b3a2294
nerbism <roadtobren@gmail.com>
2024-01-02 21:26:30 -0700

    shipment function

--- js/Section-3/02-Parameters/script.js
+++ js/Section-3/02-Parameters/script.js
@@ -9,3 +9,13 @@ function chatEnter(user) {
 
 // In this case, the Arguemnt is Bren which is the data we are passing through to the params.
 console.log(chatEnter('Bren'))
+
+
+// More examples:
+
+
+ function shipment(user, user2) {
+    return user + ' & ' + user2 + ' has been shipped!'
+ }
+
+console.log(shipment('Bren', 'D'))
