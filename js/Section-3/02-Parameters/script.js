e92c33349a33187bc17919a335a8218b5f512825
nerbism <roadtobren@gmail.com>
2024-01-02 21:19:02 -0700

    chatEnter function

---
+++ js/Section-3/02-Parameters/script.js
@@ -0,0 +1,11 @@
+// Params and Arguments
+
+
+
+// Params are placeholders where data will be passedd through
+function chatEnter(user) {
+    return user + ' has entered the chat'
+}
+
+// In this case, the Arguemnt is Bren which is the data we are passing through to the params.
+console.log(chatEnter('Bren'))
